f1c5b16fe2e944d42656898ba3db15d6ef91bd6e
mujohn26 <mujohn26@gmail.com>
2021-10-09 01:26:08 +0200

    add additioal functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC, ChangeEvent,useState} from 'react';
 import './App.css';
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>("")
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todo, setTodoList] = useState([])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value)
+    }
+    else {
+      setDeadline(Number(event.target.value))
+    }
+    
+  }
+
+  const addTaskToList = ():void => {
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='header'>
+        <div className='input-container'>
+          <input type='text' placeholder='Task...' name='task' onChange={handleChange}/>
+          <input type='number' placeholder='Deadline (in Days)...' name='deadline' onChange={handleChange}/>
+
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className='todoList'>
+
+      </div>
+
+     </div>
   );
 }
 
